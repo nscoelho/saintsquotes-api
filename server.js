46fe1eedb4b50a99424dfb1380599910ed516bdf
nscoelho <76262182+nscoelho@users.noreply.github.com>
2022-12-03 15:14:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,21 +3,18 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+const express = require("express");
+const debug = require("debug")("server");
+const app = express();
 
-const cors=require("cors");
- const corsOptions ={
-       origin:'*', 
-       response.setHeader("Access-Control-Allow-Origin", "*"),
-       credentials:true, //access-control-allow-credentials:true,
-       optionSuccessStatus:200,
-}
-httpNodeCors: {
-  origin: "*",
-  methods: "GET,PUT,POST,DELETE"
-}
-
-response.setHeader("Access-Control-Allow-Origin", "*");
- app.use(cors(corsOptions)) 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 server.use(middlewares);
 server.use(router);

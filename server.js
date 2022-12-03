9977e8069036f8976884b725dd5458b60701e257
nscoelho <76262182+nscoelho@users.noreply.github.com>
2022-12-03 20:46:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-
-const express = require("express");
-const cors = require('cors');
-const app = express();
-
 const port = process.env.PORT || 3000;
  
 app.use(
@@ -18,6 +13,27 @@ app.use(
   })
 );
 
+var express = require('express');
+var cors = require('cors');
+var app = express();
+
+var corsOptions = {
+  origin: function (origin, callback) {
+    // db.loadOrigins is an example call to load
+    // a list of origins from a backing database
+    db.loadOrigins(function (error, origins) {
+      callback(error, origins)
+    })
+  }
+}
+
+app.get('/products/:id', cors(corsOptions), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for an allowed domain.'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 server.use(middlewares);
 server.use(router);
 

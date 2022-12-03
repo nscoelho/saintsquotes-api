52e2edeac569999b270aefbe4b9d67f992831d7f
nscoelho <76262182+nscoelho@users.noreply.github.com>
2022-12-03 18:28:55 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(
+  cors({
+    origin: ["https://saintsquotes.org/"],
+    methods: ["GET", "POST", "DELETE"],
+    credentials: true,
+    origin: true,
+  })
+);
 
 server.use(middlewares);
 server.use(router);

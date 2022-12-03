c32f6ba3f4d9cb972b6d4583ce16932a481bc358
nscoelho <76262182+nscoelho@users.noreply.github.com>
2022-12-03 13:35:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+var app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 server.use(middlewares);
 server.use(router);
 
